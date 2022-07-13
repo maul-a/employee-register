a78cc6faa59198b28b1f1ab6f8237aeab437168d
Artur <artur@internet.com>
2022-07-13 16:20:25 +0200

    Backend: delete comments from employee model

--- employee-register-backend/src/models/Employee.ts
+++ employee-register-backend/src/models/Employee.ts
@@ -1,7 +1,6 @@
 import { model, Schema, Document } from 'mongoose'
 import { IAddress, AddressSchema } from '@app/models/Address'
 import { IUser, UserSchema } from './User'
-import { IComment } from './Comment'
 
 export interface IEmployee extends Document {
   firstName: string
@@ -9,7 +8,6 @@ export interface IEmployee extends Document {
   role: string
   address: IAddress
   authData: IUser
-  comments: IComment[]
 }
 
 export const EmployeeSchema = new Schema({
@@ -18,7 +16,6 @@ export const EmployeeSchema = new Schema({
   role: String,
   authData: { type: UserSchema, _id: false },
   address: { type: AddressSchema, _id: false },
-  comments: [{ type: Schema.Types.ObjectId, ref: 'comment' }]
 })
 
 const Employee = model<IEmployee>('employee', EmployeeSchema)
